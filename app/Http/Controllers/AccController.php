9d07ffedb72837dc71cd35ecfcfe90e7f91bd424
feifei <feifei@qq.com>
2018-11-29 20:00:43 +0800

    ss

--- app/Http/Controllers/AccController.php
+++ app/Http/Controllers/AccController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 
 class AccController extends Controller
 {
+	public function hook(){
+		echo exec('cd /home/wwwroot/wx.feifeifly.cn/wx/');
+		echo exec('git pull origin master');
+
+	}
     public function info()
 	{
 		
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('hook' , 'AccController@hook');
 
 Route::get('info', 'AccController@info');
 
